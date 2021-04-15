8196dc5f33f35991db55e9ad0826a1926440131b
kylebaugh <kylebaugh1@gmail.com>
2021-04-15 16:26:14 -0600

    3 of 5 complete

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,25 +11,25 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for(var key in values) {
+  console.log(values[key])
+}
 
 /*
   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+  console.log(key)
+}
 
 
 
@@ -40,7 +40,12 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  string = '';
+  for(var key in obj) {
+    console.log(obj[key])
+      string += (obj[key])
+  }
+  return string
 }
 
 
@@ -55,6 +60,14 @@ function showValues( obj ) {
 
 //Code Here
 
+function greaterThan10(obj){
+  for(let key in obj) {
+    if (obj[key] > 10)
+      obj[key] = 0
+  }
+  return obj
+}
+
 
 
 ////////// PROBLEM 3 //////////
@@ -67,6 +80,12 @@ function showValues( obj ) {
 
 //Code Here
 
+function double(obj){
+  for(let key in obj){
+    obj[key] = obj[key] * 2
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -81,6 +100,14 @@ function showValues( obj ) {
 
 //Code Here
 
+function secrets(obj){
+  string = '';
+  for(let key in obj){
+    if (key.startsWith('sh'))
+      string += obj[key]
+  }
+  return string
+}
 
 
 /* 
@@ -89,17 +116,17 @@ function showValues( obj ) {
   Uncomment the example below to see a for in loop deleting all the properties inside an object.
 */
 
-// var deleteAllThethings = {
-//   one: 1,
-//   two: 2,
-//   three: 3
-// }
+var deleteAllThethings = {
+  one: 1,
+  two: 2,
+  three: 3
+}
 
-// for(var key in deleteAllThethings) {
-//   delete deleteAllThethings[key]
-// }
+for(var key in deleteAllThethings) {
+  delete deleteAllThethings[key]
+}
 
-// console.log(deleteAllThethings)
+console.log(deleteAllThethings)
 
 
 
@@ -111,6 +138,17 @@ function showValues( obj ) {
 */
 
 //Code Here
+
+let stuffs = {
+  name: 'jeff',
+  password: '12345',
+  job: 'being dumb'
+}
+
+function removePassword(obj){
+  delete obj.password
+    return obj
+}
 
 
 
@@ -133,6 +171,12 @@ var deleteTheBigNumbers = {
 
 
 
+for(let key in deleteTheBigNumbers){
+  if (deleteTheBigNumbers[key] > 100)
+    delete deleteTheBigNumbers[key]
+}
+
+
 ////////// PROBLEM 7 //////////
 
 /*
@@ -143,6 +187,20 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
+
+let kThings = {
+  name: 'jeff',
+  password: '12345',
+  job: 'being dumb'
+}
+
+function startsWithK(obj){
+  for(let key in obj){
+    if (key.startsWith('k'))
+      delete obj[key]
+  }
+  return obj
+}
 
 
 
@@ -158,5 +216,15 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
+
+function hiddenTreasure(obj){
+  for(let key in obj){
+    if (obj[key].includes('treasure'))
+      console.log('yay')
+    else
+      delete obj[key]
+  }
+  return obj
+}
 
 
--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index, array){
+  return element % 2 === 0;
+})
 
 
 
@@ -44,8 +46,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+let postTaxPrices = prices.map(function(element, index, array){
+  return element * 1.07
+})
 
 
 ////////// PROBLEM 3 //////////
@@ -63,7 +66,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, curr){
+  return acc + curr
+})
 
 
 
@@ -89,7 +94,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(element => element.CP > 200)
 
 
 
@@ -103,10 +108,15 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate,
+     hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(element){
+  return element.price * (element.tax + 1)
+})
+  
+
 
 
 
@@ -126,6 +136,26 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let asdf = [];
+let compilation = purchases.map(function(element){
+  if (element.owner === "Bob")
+    asdf.push(element.price)
+    })
+
+console.log(asdf)
+
+let bobsTotal = asdf.reduce(function(acc, curr){
+    return acc + curr})
+
+console.log(bobsTotal)
+  
+
+
+// let bobsTotal = compilation.reduce(acc, curr)
+//     return acc + curr
+
+// let totalPopulation = populations.reduce(function(acc, curr){
+//   return acc + curr
+// })
 
 
